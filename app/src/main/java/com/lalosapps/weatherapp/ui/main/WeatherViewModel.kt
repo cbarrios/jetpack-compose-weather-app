7916abc54a72accab1b775c24d778938056ec83e
cbarrios <lalobarrios16@yahoo.com>
2022-07-04 00:24:33 -0400

    master: ui state and view model

---
+++ app/src/main/java/com/lalosapps/weatherapp/ui/main/WeatherViewModel.kt
@@ -0,0 +1,56 @@
+package com.lalosapps.weatherapp.ui.main
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lalosapps.weatherapp.core.util.Result
+import com.lalosapps.weatherapp.domain.location.LocationTracker
+import com.lalosapps.weatherapp.domain.repository.WeatherRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WeatherViewModel @Inject constructor(
+    private val repository: WeatherRepository,
+    private val locationTracker: LocationTracker
+) : ViewModel() {
+
+    var state by mutableStateOf(WeatherState())
+        private set
+
+    fun loadWeatherInfo() {
+        viewModelScope.launch {
+            state = state.copy(
+                isLoading = true,
+                error = null
+            )
+            locationTracker.getCurrentLocation()?.let {
+                when (val result = repository.getWeatherData(it.latitude, it.longitude)) {
+                    is Result.Success -> {
+                        state = state.copy(
+                            weatherInfo = result.data,
+                            isLoading = false,
+                            error = null
+                        )
+                    }
+                    is Result.Error -> {
+                        state = state.copy(
+                            weatherInfo = null,
+                            isLoading = false,
+                            error = result.message
+                        )
+                    }
+                }
+            } ?: kotlin.run {
+                state = state.copy(
+                    weatherInfo = null,
+                    isLoading = false,
+                    error = "Couldn't retrieve location. Make sure to grant permission and enable GPS."
+                )
+            }
+        }
+    }
+}
