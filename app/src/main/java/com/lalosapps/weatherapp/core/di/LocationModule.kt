bd5a1cf86bb6dacec8889dd88659ca6e97b03397
cbarrios <lalobarrios16@yahoo.com>
2022-07-04 00:05:28 -0400

    master: di modules

---
+++ app/src/main/java/com/lalosapps/weatherapp/core/di/LocationModule.kt
@@ -0,0 +1,18 @@
+package com.lalosapps.weatherapp.core.di
+
+import com.lalosapps.weatherapp.core.location.DefaultLocationTracker
+import com.lalosapps.weatherapp.domain.location.LocationTracker
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(defaultLocationTracker: DefaultLocationTracker): LocationTracker
+}
---
+++ app/src/main/java/com/lalosapps/weatherapp/core/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.lalosapps.weatherapp.core.di
+
+import com.lalosapps.weatherapp.data.repository.WeatherRepositoryImpl
+import com.lalosapps.weatherapp.domain.repository.WeatherRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(
+        weatherRepositoryImpl: WeatherRepositoryImpl
+    ): WeatherRepository
+}
