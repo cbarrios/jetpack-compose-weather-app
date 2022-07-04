f6720e47a32b9fbb8892e0b3c231199b430e3c1c
cbarrios <lalobarrios16@yahoo.com>
2022-07-03 22:46:33 -0400

    master: hilt config

--- app/src/main/java/com/lalosapps/weatherapp/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/lalosapps/weatherapp/data/repository/WeatherRepositoryImpl.kt
@@ -1,15 +1,24 @@
 package com.lalosapps.weatherapp.data.repository
 
+import com.lalosapps.weatherapp.core.mappers.toWeatherInfo
 import com.lalosapps.weatherapp.core.util.Result
 import com.lalosapps.weatherapp.data.remote.WeatherApi
 import com.lalosapps.weatherapp.domain.repository.WeatherRepository
 import com.lalosapps.weatherapp.domain.weather.WeatherInfo
+import javax.inject.Inject
 
-class WeatherRepositoryImpl(
-    private val api: WeatherApi // use Hilt next...
-) : WeatherRepository {
+class WeatherRepositoryImpl @Inject constructor(private val api: WeatherApi) : WeatherRepository {
     override suspend fun getWeatherData(lat: Double, long: Double): Result<WeatherInfo> {
-        TODO("Not yet implemented")
+        return try {
+            Result.Success(
+                api.getWeatherData(
+                    lat = lat,
+                    long = long
+                ).toWeatherInfo()
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Result.Error(e.message ?: "An unknown error occurred.")
+        }
     }
-
 }
