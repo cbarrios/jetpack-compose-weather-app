6920a6b7c9738948ce122767cbe3bb1ecfbfd43f
cbarrios <lalobarrios16@yahoo.com>
2022-07-04 11:49:16 -0400

    master: ui weather card + @field:Json annotation fix

--- app/src/main/java/com/lalosapps/weatherapp/data/remote/WeatherDataDto.kt
+++ app/src/main/java/com/lalosapps/weatherapp/data/remote/WeatherDataDto.kt
@@ -4,10 +4,10 @@ import com.squareup.moshi.Json
 
 data class WeatherDataDto(
     val time: List<String>,
-    @Json(name = "temperature_2m") val temperatures: List<Double>,
-    @Json(name = "weathercode") val weatherCodes: List<Int>,
-    @Json(name = "pressure_msl") val pressures: List<Double>,
-    @Json(name = "windspeed_10m") val windSpeeds: List<Double>,
-    @Json(name = "relativehumidity_2m") val humidities: List<Double>
+    @field:Json(name = "temperature_2m") val temperatures: List<Double>,
+    @field:Json(name = "weathercode") val weatherCodes: List<Int>,
+    @field:Json(name = "pressure_msl") val pressures: List<Double>,
+    @field:Json(name = "windspeed_10m") val windSpeeds: List<Double>,
+    @field:Json(name = "relativehumidity_2m") val humidities: List<Double>
 )
 
--- app/src/main/java/com/lalosapps/weatherapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/lalosapps/weatherapp/ui/main/MainActivity.kt
@@ -1,45 +1,54 @@
 package com.lalosapps.weatherapp.ui.main
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.lalosapps.weatherapp.ui.theme.WeatherAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: WeatherViewModel by viewModels()
+
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            viewModel.loadWeatherInfo()
+        }
+        permissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
         setContent {
             WeatherAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        WeatherCard(
+                            state = viewModel.state,
+                            backgroundColor = MaterialTheme.colors.background
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WeatherAppTheme {
-        Greeting("Android")
     }
 }
